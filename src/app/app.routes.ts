2a0ab9616ccd4b4420c4ce7613882c4062b35cd8
Benjamin Elsner <benny.nemeth@gmail.com>
2025-01-28 12:23:36 +0100

    first route added

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,7 @@
 import { Routes } from '@angular/router';
+import { ToDoListsComponent } from './lists/to-do-lists.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: '', pathMatch: 'full' },
+  { path: 'lists', component: ToDoListsComponent },
+];
---
+++ src/app/lists/to-do-lists.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-to-do-lists',
+  standalone: true,
+  imports: [],
+  templateUrl: './to-do-lists.component.html',
+  styles: ``
+})
+export class ToDoListsComponent {
+
+}
