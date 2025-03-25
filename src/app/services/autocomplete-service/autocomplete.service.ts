1fb37e7a43546c5659ed403b33acb335b3973826
Benjamin Elsner <benny.nemeth@gmail.com>
2025-03-25 08:26:54 +0100

    chore: Add more datasets

--- src/app/services/autocomplete-service/autocomplete.service.ts
+++ src/app/services/autocomplete-service/autocomplete.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import * as tf from '@tensorflow/tfjs';
-// import * as use from '@tensorflow-models/universal-sentence-encoder';
+import * as use from '@tensorflow-models/universal-sentence-encoder';
 
 @Injectable({
   providedIn: 'root',
@@ -9,12 +9,26 @@ export class AutocompleteService {
   private model: tf.Sequential = tf.sequential();
   private trainingData: { input: string; output: string }[] = [
     { input: 'Put', output: 'Putzen' },
-    { input: 'Ein', output: 'Einkaufen' },
-    { input: 'Wäs', output: 'Wäsche Waschen' },
-    { input: 'Koc', output: 'Kochen' },
-    { input: 'Auf', output: 'Aufräumen' },
+    { input: 'Eink', output: 'Einkaufen' },
+    { input: 'Wäsc', output: 'Wäsche Waschen' },
+    { input: 'Koch', output: 'Kochen' },
+    { input: 'Aufr', output: 'Aufräumen' },
     { input: 'Spü', output: 'Spülen' },
     { input: 'Küc', output: 'Küche Putzen' },
+    { input: 'Putze', output: 'Putzen' },
+    { input: 'Einkauf', output: 'Einkaufen' },
+    { input: 'Wäsche ', output: 'Wäsche Waschen' },
+    { input: 'Koche', output: 'Kochen' },
+    { input: 'Aufräu', output: 'Aufräumen' },
+    { input: 'Spüle', output: 'Spülen' },
+    { input: 'Küche P', output: 'Küche Putzen' },
+    { input: 'Putzen', output: 'Putzen' },
+    { input: 'Einkaufen', output: 'Einkaufen' },
+    { input: 'Wäsche Waschen', output: 'Wäsche Waschen' },
+    { input: 'Kochen', output: 'Kochen' },
+    { input: 'Aufräumen', output: 'Aufräumen' },
+    { input: 'Spülen', output: 'Spülen' },
+    { input: 'Küche Putzen', output: 'Küche Putzen' },
   ];
   private encoder: any;
 
@@ -23,44 +37,32 @@ export class AutocompleteService {
   }
 
   async loadEncoder() {
-    // this.encoder = await use.load();
+    this.encoder = await use.load();
     this.initModel();
-    await this.trainModel();
   }
 
   async initModel() {
     this.model.add(
-      tf.layers.dense({ inputShape: [512], units: 64, activation: 'relu' })
+      tf.layers.dense({ units: 64, inputShape: [512], activation: 'relu' })
     );
-    this.model.add(tf.layers.dropout({ rate: 0.3 }));
-    this.model.add(
-      tf.layers.dense({
-        units: this.trainingData.length,
-        activation: 'softmax',
-      })
-    );
-    this.model.add(tf.layers.dense({ units: 32, activation: 'relu' }));
-    this.model.add(tf.layers.dense({ units: 128, activation: 'relu' }));
+    // this.model.add(tf.layers.dense({ units: 32, activation: 'relu' }));
     this.model.add(tf.layers.dense({ units: 512 }));
-    this.model.summary(); // Debugging
-    this.model.compile({
-      loss: 'categoricalCrossentropy',
-      optimizer: 'adam',
-      metrics: ['accuracy'],
-    });
+    this.model.compile({ loss: 'meanSquaredError', optimizer: 'adam' });
     console.log('KI Model bereit!');
   }
 
-  async trainModel() {
+  async train(newData: { input: string; output: string }) {
     if (!this.encoder) {
       console.warn('Encoder nicht geladen');
       return;
     }
 
+    this.trainingData.push(newData);
+
     const xs = await this.encoder.embed(this.trainingData.map((d) => d.input));
     const ys = await this.encoder.embed(this.trainingData.map((d) => d.output));
 
-    await this.model.fit(xs, ys, { epochs: 20, shuffle: true });
+    await this.model.fit(xs, ys, { epochs: 15 });
     console.log('KI trainiert mit neuen Daten!', this.trainingData);
   }
 
@@ -83,12 +85,7 @@ export class AutocompleteService {
     if (!this.encoder) return;
 
     const inputEmbedding = await this.encoder.embed([input]);
-    console.log(
-      'Eingabe:',
-      input,
-      'Shape:',
-      inputEmbedding.reshape([512]).shape
-    );
+    console.log('Eingabe:', input, 'Shape:', inputEmbedding.shape);
 
     const inputTensor = inputEmbedding.reshape([512]); // Falls nötig, in 1D umwandeln
     console.log('Eingabe Vektor:', await inputTensor.data());
@@ -106,7 +103,7 @@ export class AutocompleteService {
 
   async closestMatch(embedding: Float32Array): Promise<string> {
     let bestMatch = '';
-    let bestScore = Infinity;
+    let bestScore = -Infinity;
 
     for (const data of this.trainingData) {
       const outputEmbedding = await this.encoder.embed([data.output]);
@@ -121,7 +118,7 @@ export class AutocompleteService {
         .cosineDistance(outputTensor, inputTensor, 0)
         .dataSync()[0];
 
-      if (score < bestScore) {
+      if (score > bestScore) {
         bestMatch = data.output;
         bestScore = score;
       }
