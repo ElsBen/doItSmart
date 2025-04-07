8883b833a814ed597acfe0182f245230902fdd04
Benjamin Elsner <benny.nemeth@gmail.com>
2025-04-07 15:38:22 +0200

    chore: Outsource prediction HTML template - Created `prediction.component.ts` to encapsulate prediction logic and view - Moved and adjusted the HTML content into a dedicated component - Added `@Input` to display prediction data dynamically - Added `@Output` to emit selected prediction events to the parent component

---
+++ src/app/prediction/prediction.component.ts
@@ -0,0 +1,23 @@
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+
+@Component({
+  selector: 'app-prediction',
+  imports: [],
+  template: `
+    <div class="list-group" style="width: max-content; cursor: pointer">
+      <a
+        class="list-group-item list-group-item-action list-group-item-success"
+        (click)="selectPrediction()"
+        >{{ prediction }}</a
+      >
+    </div>
+  `,
+  styles: ``,
+})
+export class PredictionComponent {
+  @Input() prediction: string = '';
+
+  @Output() predictionSelected: EventEmitter<string> =
+    new EventEmitter<string>();
+  selectPrediction() {}
+}
