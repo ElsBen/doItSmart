83bb69d7a48dbb034101656665e7715ee7133df2
Benjamin Elsner <benny.nemeth@gmail.com>
2025-04-15 13:04:34 +0200

    feat: Add deadline reminder notification to `deadline-reminder.service`
    - Injected `NotificationService` into `deadline-reminder.service`
    - Created `remindIfDeadlineApproaching` to check time difference between current date and selected date
    - Added `getDisplayNotificationMessage` to request permission and forward message to `displayNotification`

--- src/app/services/deadline-reminder-service/deadline-reminder.service.ts
+++ src/app/services/deadline-reminder-service/deadline-reminder.service.ts
@@ -1,11 +1,15 @@
 import { Injectable } from '@angular/core';
 import { DateService } from '../date-service/date.service';
+import { NotificationService } from '../notification-service/notification.service';
 
 @Injectable({
   providedIn: 'root',
 })
 export class DeadlineReminderService {
-  constructor(private dateService: DateService) {}
+  constructor(
+    private dateService: DateService,
+    private notificationService: NotificationService
+  ) {}
 
   isDeadlineCloseToCurrentDate(deadline: string): string {
     const currentDate = this.getCurrentDate();
@@ -14,6 +18,7 @@ export class DeadlineReminderService {
     selectedDate.setHours(0, 0, 0, 0);
 
     const timeDifference = this.calcTimeDifference(currentDate, selectedDate);
+    this.remindIfDeadlineApproaching(deadline, timeDifference);
 
     if (timeDifference < 0) {
       return 'bg-secondary';
@@ -38,5 +43,22 @@ export class DeadlineReminderService {
     return Math.ceil(
       (selectedDate.getTime() - currentDate.getTime()) / (1000 * 60 * 60 * 24)
     );
+  }
+
+  remindIfDeadlineApproaching(deadline: string, difference: number) {
+    if (difference <= 2 && difference >= 0) {
+      this.getDisplayNotificationMessage(
+        `Der Termin "${deadline}" rückt näher!`
+      );
+    } else if (difference < 0) {
+      this.getDisplayNotificationMessage(
+        `Der Termin "${deadline}" ist abgelaufen!`
+      );
+    }
+  }
+
+  private getDisplayNotificationMessage(message: string) {
+    this.notificationService.requestNotificationPermission();
+    this.notificationService.displayNotification('Deadline Reminder', message);
   }
 }
