9ab83ec571e018a31281db9dcd564d98821259cf
Benjamin Elsner <benny.nemeth@gmail.com>
2025-04-15 08:57:23 +0200

    build: Add PWA configuration and service worker setup

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,19 +1,23 @@
 import {
   ApplicationConfig,
   importProvidersFrom,
-  provideZoneChangeDetection,
+  provideZoneChangeDetection, isDevMode,
 } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { provideAnimations } from '@angular/platform-browser/animations';
 
 import { routes } from './app.routes';
 import { HttpClientModule } from '@angular/common/http';
+import { provideServiceWorker } from '@angular/service-worker';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
     provideAnimations(),
-    importProvidersFrom(HttpClientModule),
+    importProvidersFrom(HttpClientModule), provideServiceWorker('ngsw-worker.js', {
+            enabled: !isDevMode(),
+            registrationStrategy: 'registerWhenStable:30000'
+          }),
   ],
 };
