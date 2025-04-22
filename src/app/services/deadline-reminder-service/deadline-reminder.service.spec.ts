195382ac8da66a451fcdc7e46f6d8283d42bcccd
Benjamin Elsner <benny.nemeth@gmail.com>
2025-04-22 09:37:57 +0200

    test(deadline-reminder): Improve test date generation
    - Refactored test data setup for more accurate and maintainable date-based tests

--- src/app/services/deadline-reminder-service/deadline-reminder.service.spec.ts
+++ src/app/services/deadline-reminder-service/deadline-reminder.service.spec.ts
@@ -1,13 +1,24 @@
 import { TestBed } from '@angular/core/testing';
 
 import { DeadlineReminderService } from './deadline-reminder.service';
+import { DateService } from '../date-service/date.service';
 
 describe('DeadlineReminderService', () => {
   let service: DeadlineReminderService;
+  let dateService: DateService;
+
+  function setTestDate(amountDays: number): string {
+    const currentDate = new Date();
+    const desiredDate = new Date().setDate(
+      currentDate.getDate() + amountDays
+    ) as unknown as string;
+    return dateService.convertDateToLocalDate(desiredDate);
+  }
 
   beforeEach(() => {
     TestBed.configureTestingModule({});
     service = TestBed.inject(DeadlineReminderService);
+    dateService = TestBed.inject(DateService);
   });
 
   it('should be created', () => {
@@ -18,7 +29,7 @@ describe('DeadlineReminderService', () => {
     spyOn(service as any, 'getDisplayNotificationMessage');
 
     // 2 Tage in der Zukunft
-    const deadline = '23.4.2025, 10:00:00';
+    const deadline = setTestDate(2);
     const entryName = 'Test Entry';
 
     service.remindIfDeadlineApproaching(deadline, entryName);
@@ -32,7 +43,7 @@ describe('DeadlineReminderService', () => {
     spyOn(service as any, 'getDisplayNotificationMessage');
 
     // Mehr als 2 Tage in der Zukunft
-    const deadline = '1.5.2026, 10:00:00';
+    const deadline = setTestDate(20);
     const entryName = 'Test Entry';
 
     service.remindIfDeadlineApproaching(deadline, entryName);
