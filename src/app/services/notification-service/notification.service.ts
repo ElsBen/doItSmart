5c2f69109f8d6b877d99ef527d414d9c19826d4a
Benjamin Elsner <benny.nemeth@gmail.com>
2025-04-24 10:55:05 +0200

    chore: Remove unused element

--- src/app/services/notification-service/notification.service.ts
+++ src/app/services/notification-service/notification.service.ts
@@ -67,7 +67,6 @@ export class NotificationService {
 
   displayNotification(title: string, body: string) {
     if (Notification.permission === 'granted') {
-      // new Notification(title, { body });
       navigator.serviceWorker.getRegistration().then((registration) => {
         if (registration) {
           registration.showNotification(title, {
