426a3be4b5d7f35516634c30948b7078ca3e1ce1
Benjamin Elsner <benny.nemeth@gmail.com>
2025-05-02 20:01:19 +0200

    feat(calendar-feature): Create `calendar-wrapper` component and update `calendar.component`
    - Created `calendar-wrapper` component to encapsulate calendar logic or layout
    - Adjusted `calendar.component` to use or integrate with `calendar-wrapper`

--- src/app/calendar-view/calendar.component.ts
+++ src/app/calendar-view/calendar.component.ts
@@ -1,29 +1,29 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { CalendarWrapperModule } from './calendar-wrapper.module';
 import { CalendarEvent, CalendarModule, CalendarView } from 'angular-calendar';
 
 @Component({
   selector: 'app-calendar',
-  imports: [CommonModule, CalendarModule],
+  imports: [CommonModule, CalendarWrapperModule, CalendarModule],
   template: `<div class="calendar-wrapper">
-    <mwl-calendar
-      [view]="view"
+    <mwl-calendar-month-view
       [viewDate]="viewDate"
       [events]="events"
       [locale]="'de'"
     >
-    </mwl-calendar>
+    </mwl-calendar-month-view>
   </div>`,
   styles: `
-  .calendar-wrapper {
-  max-width: 1000px;
-  height: 100%;
-  margin: 2rem auto;
-  padding: 1rem;
-  background: white;
-  border-radius: 8px;
-  box-shadow: 0 2px 10px rgba(0, 0, 0, 0.1);
-}
+//   .calendar-wrapper {
+//   max-width: 1000px;
+//   height: 100%;
+//   margin: 2rem auto;
+//   padding: 1rem;
+//   background: white;
+//   border-radius: 8px;
+//   box-shadow: 0 2px 10px rgba(0, 0, 0, 0.1);
+// }
   `,
 })
 export class CalendarComponent {
