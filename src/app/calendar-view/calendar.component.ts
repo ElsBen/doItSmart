8272a2e7d85256b38e5390dcc03938fc4b51c986
Benjamin Elsner <benny.nemeth@gmail.com>
2025-05-02 12:04:58 +0200

    feat(calendar-feature): Create calendar build and import

--- src/app/calendar-view/calendar.component.ts
+++ src/app/calendar-view/calendar.component.ts
@@ -1,15 +1,39 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { CalendarEvent, CalendarModule, CalendarView } from 'angular-calendar';
 
 @Component({
   selector: 'app-calendar',
-  imports: [],
-  template: `
-    <p>
-      calendar works!
-    </p>
+  imports: [CommonModule, CalendarModule],
+  template: `<div class="calendar-wrapper">
+    <mwl-calendar
+      [view]="view"
+      [viewDate]="viewDate"
+      [events]="events"
+      [locale]="'de'"
+    >
+    </mwl-calendar>
+  </div>`,
+  styles: `
+  .calendar-wrapper {
+  max-width: 1000px;
+  height: 100%;
+  margin: 2rem auto;
+  padding: 1rem;
+  background: white;
+  border-radius: 8px;
+  box-shadow: 0 2px 10px rgba(0, 0, 0, 0.1);
+}
   `,
-  styles: ``
 })
 export class CalendarComponent {
+  view: CalendarView = CalendarView.Month;
+  viewDate: Date = new Date();
 
+  events: CalendarEvent[] = [
+    {
+      start: new Date(),
+      title: 'Meine erste Aufgabe',
+    },
+  ];
 }
