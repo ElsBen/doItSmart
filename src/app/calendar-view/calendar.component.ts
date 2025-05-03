895c0ea851d8a2e0d8447a36f96100ac217beadf
Benjamin Elsner <benny.nemeth@gmail.com>
2025-05-03 13:30:41 +0200

    feat(calendar-feature): Remove entire `fullCalendar` packages and elements due to compatibility issues with Angular 19 - Reason: Incompatibility with Angular 19

--- src/app/calendar-view/calendar.component.ts
+++ src/app/calendar-view/calendar.component.ts
@@ -4,17 +4,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-calendar',
   imports: [CommonModule],
-  template: ``,
-  styles: `
-//   .calendar-wrapper {
-//   max-width: 1000px;
-//   height: 100%;
-//   margin: 2rem auto;
-//   padding: 1rem;
-//   background: white;
-//   border-radius: 8px;
-//   box-shadow: 0 2px 10px rgba(0, 0, 0, 0.1);
-// }
-  `,
+  template: ` <div id="calendar"></div> `,
+  styles: ``,
 })
 export class CalendarComponent {}
