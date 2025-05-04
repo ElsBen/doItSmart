2ef6cefcfadf860e3865204e0757d5df01267f12
Benjamin Elsner <benny.nemeth@gmail.com>
2025-05-04 17:12:26 +0200

    feat(calendar-feat): Remove `fullCalendar` packages and elements

--- src/app/calendar-view/calendar.component.ts
+++ src/app/calendar-view/calendar.component.ts
@@ -1,32 +1,11 @@
 import { CommonModule } from '@angular/common';
-import { Component, signal } from '@angular/core';
-import { FullCalendarModule } from '@fullcalendar/angular'; // must go before plugins
-import { CalendarOptions } from '@fullcalendar/core/index.js';
-import dayGridPlugin from '@fullcalendar/daygrid/index.js';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-calendar',
-  imports: [CommonModule, FullCalendarModule],
-  template: `
-    <!-- <div class="m-4">
-      <h1>Demo App</h1>
-      <full-calendar [options]="calendarOptions"></full-calendar>
-    </div> -->
-    <full-calendar [options]="calendarOptions">
-      <ng-template #eventContent let-arg>
-        <b>{{ arg.timeText }}</b>
-        <i>{{ arg.event.title }}</i>
-      </ng-template>
-    </full-calendar>
-  `,
+  standalone: true,
+  imports: [CommonModule],
+  template: ``,
   styles: ``,
 })
-export class CalendarComponent {
-  calendarVisible = signal(true);
-  calendarOptions: CalendarOptions = {
-    plugins: [dayGridPlugin],
-    initialView: 'dayGridMonth',
-    weekends: false,
-    events: [{ title: 'Meeting', start: new Date() }],
-  };
-}
+export class CalendarComponent {}
