f45340491a419e6a098afe63314530cd582212e7
Benjamin Elsner <benny.nemeth@gmail.com>
2025-05-07 10:41:41 +0200

    feat(calendar-feature): Remove `FullCalendar` import and `calendar.wrapper.module`

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -10,14 +10,13 @@ import { provideAnimations } from '@angular/platform-browser/animations';
 import { routes } from './app.routes';
 import { HttpClientModule } from '@angular/common/http';
 import { provideServiceWorker } from '@angular/service-worker';
-import { FullCalendarModule } from '@fullcalendar/angular';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
     provideAnimations(),
-    importProvidersFrom(HttpClientModule, FullCalendarModule),
+    importProvidersFrom(HttpClientModule),
     provideServiceWorker('ngsw-worker.js', {
       enabled: !isDevMode(),
       registrationStrategy: 'registerWhenStable:30000',
